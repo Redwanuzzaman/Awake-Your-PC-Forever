75a4a984edfecf4a5ad1c35c4578159c205f046d
Md Redwanuzzaman <iamredwanuzzaman@gmail.com>
2020-09-13 23:56:17 +0600

    Update Awake.py

--- Awake.py
+++ Awake.py
@@ -4,7 +4,7 @@ import time
 pyautogui.FAILSAFE = False
 
 while True:
-    time.sleep(300)
+    time.sleep(300)  # a break of 5 minutes
     for i in range(100):
         pyautogui.moveTo(0, i * 5)
     for i in range(3):
